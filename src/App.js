9add66ec0f6ba78b8f6f47f18958824344b327fd
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-04-22 00:50:30 +0600

    changeset: setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AboutScreen from "./screens/AboutScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about" component={AboutScreen} />
+        <Route exact path="/" component={HomeScreen} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
