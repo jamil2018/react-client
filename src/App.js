a156c79d0f60048a2409082a984dc6f405c1ee48
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-04-22 02:04:43 +0600

    changeset: setup react-query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import AboutScreen from "./screens/AboutScreen";
 import HomeScreen from "./screens/HomeScreen";
 import NotFoundScreen from "./screens/NotFoundScreen";
 import theme from "./theme/applicationTheme";
+import queryClient from "./queryClient";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/about" component={AboutScreen} />
-          <Route exact path="/" component={HomeScreen} />
-          <Route component={NotFoundScreen} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/about" component={AboutScreen} />
+            <Route exact path="/" component={HomeScreen} />
+            <Route component={NotFoundScreen} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
